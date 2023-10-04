6deaff33d54e134f076619feb613452bc967e703
Frank Casanova <frankcasanova.info@gmail.com>
2023-10-04 14:15:36 +0200

    refactor and some impruvements included, readme wirted

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# time-to-load-website
-This script takes a url from the user and returns the time taken to load that website.
+# Website Load Time Checker
+
+This Python script measures the time taken to load a website by requesting the URL and calculating the time difference. It uses the `urlopen` function from the `urllib.request` module to open the URL and the `time` module to measure the time.
+
+## Usage
+
+1. Make sure you have Python installed on your system.
+2. Clone the repository or download the script.
+3. Install the required dependencies using `pip install -r requirements.txt`.
+4. Run the script using `python load_time_checker.py`.
+5. Enter the URL for which you want to measure the load time when prompted.
+6. The script will display the time taken to load the website in seconds.
+
+## Example
+
+```bash
+py main.py
+Enter the URL whose loading time you want to check: www.example.com
+The time taken to load www.example.com is 0.76 seconds.
+```
+## Contributing
+
+Contributions are welcome! If you have any suggestions or improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License]
+
+Feel free to modify the content according to your project's specific details and requirements.
+
+Documented by Frank Casanova
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from urllib.request import urlopen
 import time
-def get_load_time(url):
+
+def get_load_time(url: str) -> float:
     """
-    This function takes a user defined url as imput 
-    and returns the time taken to load that url in seconds.
-    
-    Args: url (string) The user defined url.
-    
-    Returns: time_to_load (float) The time takne to load the website in seconds.
+    This function takes a user-defined URL as input and returns the time taken to load that URL in seconds.
+    Args:
+        url (str): The user-defined URL.
+    Returns:
+        float: The time taken to load the website in seconds.
     """
-    
-    if ('https' or 'http') in url: 
+    if 'https' in url or 'http' in url: 
         open_this_url = urlopen(url)
     else:
         open_this_url = urlopen('https://' + url)
@@ -18,16 +17,12 @@ def get_load_time(url):
     start_time = time.time()
     open_this_url.read()
     end_time = time.time()
+    
     open_this_url.close()
     
     time_to_load = end_time - start_time
-    
     return time_to_load
 
-
 if __name__ == "__main__":
-    url = input("Enter the url whose loading time you want to check: ")
-    print(f'\nThe time taken to load {url} is {get_load_time(url):.2} seconds.')
-
-
-    
+    url = input("Enter the URL whose loading time you want to check: ")
+    print(f'\nThe time taken to load {url} is {get_load_time(url):.2f} seconds.')
